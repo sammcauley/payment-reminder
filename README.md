7a16ec181e76eee8e31a29df813ac41801038a37
sammcauley <55258446+sammcauley@users.noreply.github.com>
2025-05-21 16:54:25 +0100

    Google api/terraform (#11)
    * create terraform files
    
    * set up terraform
    
    * bugfix terraform
    
    * update readme
    
    * update readme
    
    ---------
    
    Co-authored-by: admin <http://admin@192.168.0.10/Bonobo.Git.Server/DataTool.git>

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# payment-reminder
+# Payment Reminder
+
+## Project setup instructions
+### 1. Prerequisites
+ - Install Terraform
+ - Install Git
+ - Install gcloud CLI
+
+ ### 2. Clone the repository
+ ```bash
+ git clone https://github.com/sammcauley/payment-reminder.git
+ cd payment-reminder
+ ```
+
+ ### 3. Setup Google Cloud Project
+ 1. Create a new Google Cloud project via the console
+ 2. Enable billing
+
+ ### 4. Inital authentication
+ `gcloud auth application-default login`
+
+ ### 5. Run Terraform to create resources
+ ```bash
+ cd terraform
+ terraform init
+ terraform apply
+ ```
+
+ ### 6. Save and set service account credentials
+`source save-key-set-env.sh`
+<br>Optionally you can add the env variable in your .bashrc
---
+++ terraform/main.tf
@@ -0,0 +1,33 @@
+provider "google" {
+    project = var.project_id
+    region = "europe-west2"
+}
+
+resource "google_project_service" "sheets_api" {
+    project = var.project_id
+    service = "sheets.googleapis.com"
+}
+
+resource "google_service_account" "sheets_service_account" {
+    account_id = "sheets-access"
+    display_name = "Sheets Access"
+}
+
+resource "google_service_account_key" "sheets_key" {
+    service_account_id = google_service_account.sheets_service_account.name
+    private_key_type = "TYPE_GOOGLE_CREDENTIALS_FILE"
+}
+
+resource "local_file" "sheets_key_file" {
+  content  = base64decode(google_service_account_key.sheets_key.private_key)
+  filename = "${path.module}/sheets-key.json"
+}
+
+output "sheets_service_account_email" {
+    value = google_service_account.sheets_service_account.email
+}
+
+output "sheets_key_json" {
+    sensitive = true
+    value = google_service_account_key.sheets_key.private_key
+}

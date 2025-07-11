61b9a95478aa4d503378a0f9ce97cbb8499beb7d
Sam <se.mcauley1@gmail.com>
2025-07-11 16:36:26 +0100

    cloud run functions

--- main.py
+++ main.py
@@ -1,43 +1,37 @@
 from services.sheets_service import read_sheet
 from services.reminder_service import build_reminder_message_html
 from services.email_service import send_reminders_to_all
-from utils.secrets import get_gmail_password, get_gmail_sender, get_spreadsheet_id, get_spreadsheet_range, get_recipients
+from utils.secrets import get_gmail_password, get_gmail_sender, get_spreadsheet_id, get_spreadsheet_range
+from flask import Flask, jsonify
 
-SPREADSHEET_ID = get_spreadsheet_id()
-RANGE = get_spreadsheet_range()
-EMAIL_RECIPIENTS = get_recipients()
-SENDER_EMAIL = get_gmail_sender()
-SENDER_PASSWORD = get_gmail_password()
+app = Flask(__name__)
 
-
-def main():
+@app.route("/", methods=["GET"])
+def run_reminder():
     try:
+        SPREADSHEET_ID = get_spreadsheet_id()
+        RANGE = get_spreadsheet_range()
+        SENDER_EMAIL = get_gmail_sender()
+        SENDER_PASSWORD = get_gmail_password()
+
         sheet_data = read_sheet(spreadsheet_id=SPREADSHEET_ID, range_name=RANGE)
+        html_result = build_reminder_message_html
+
+        if not html_result:
+            return jsonify({"status": "No payments due"}), 200
+
+        html_message, pay_date = html_message
+        subject = f"💰 Upcoming Payment on {pay_date}"
+
+        send_reminders_to_all(
+            html_message=html_message,
+            subject=subject,
+            sender_email=SENDER_EMAIL,
+            sender_password=SENDER_PASSWORD
+        )
+
+        return jsonify({"status": "Emails sent successfully"}), 200
+    
     except Exception as e:
-        print(f"Failed to return sheet data: {e}")
-        return
-    
-    try:
-        html_message = build_reminder_message_html(sheet_data)
-    except Exception as e:
-        print(f"Failed to build html reminder message: {e}")
-        return
-    
-    if not html_message:
-        print("No payments due in 2 days. Exiting.")
-        return
-    
-    html_message, pay_date = html_message
-
-    subject = f"💰 Upcoming Payment on {pay_date}"
-
-    send_reminders_to_all(
-        html_message=html_message,
-        subject=subject,
-        sender_email=SENDER_EMAIL,
-        sender_password=SENDER_PASSWORD
-    )
-
-
-if __name__ == "__main__":
-    main()
+        print(f"Error occurred: {e}")
+        return jsonify({"error": str(e)}), 500
